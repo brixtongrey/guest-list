5ea2f2f159a0554ccda764a073a976a6dc5bb72d
Brixtongrey <brixtonaruda@gmail.com>
2025-09-20 08:34:38 -0600

    feat: created async loadSingleGuest()

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
-import { getAllGuests } from "./api/guests";
+import { use, useEffect, useState } from "react";
+import { getAllGuests, getGuestById } from "./api/guests";
 import GuestList from "./components/GuestList";
 
 function App() {
 
   const [guests, setGuests] = useState([]);
+  const [selectedGuest, setSelectedGuest] = useState(null);
+
 
 
 //fetch all Guests on mount
   useEffect(() => {
-
     async function loadGuests() {
       try {
         const data = await getAllGuests();
@@ -21,6 +22,20 @@ function App() {
     }
     loadGuests();
   }, []);
+
+
+// fetch single guest
+useEffect (() => {
+  async function loadSingleGuest() {
+    try {
+      const data = await getGuestById(1365);
+      setSelectedGuest(data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  loadSingleGuest();
+}, [1365])
 
 
   return (
