7ec99eb617cc895751887fdffd09e4affc32a18e
Brixtongrey <brixtonaruda@gmail.com>
2025-09-20 07:59:48 -0600

    feat: created useEffect loadGuests()

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllGuests } from "./api/guests";
 
 function App() {
+
+  const [guests, setGuests] = useState([]);
+
+//fetch all Guests on mount
+  useEffect(() => {
+
+    async function loadGuests() {
+      try {
+        const data = await getAllGuests();
+        console.log("data in guests", data);
+        setGuests(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadGuests();
+  }, []);
+
+
   return (
   <>
-  <h1>App</h1>
+  <h1>Guests</h1>
   </>
 );
 }
