7d71e311531499585664d98346fd2c5887e8fec2
Brixtongrey <brixtonaruda@gmail.com>
2025-09-20 08:39:58 -0600

    feat: added setSelectedId to GuestList

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import { getAllGuests, getGuestById } from "./api/guests";
 import GuestList from "./components/GuestList";
 
 function App() {
-
   const [guests, setGuests] = useState([]);
   const [selectedGuest, setSelectedGuest] = useState(null);
+  const [selectedId, setSelectedId] = useState(null);
 
-
-
-//fetch all Guests on mount
+  //fetch all Guests on mount
   useEffect(() => {
     async function loadGuests() {
       try {
@@ -23,27 +21,26 @@ function App() {
     loadGuests();
   }, []);
 
-
-// fetch single guest
-useEffect (() => {
-  async function loadSingleGuest() {
-    try {
-      const data = await getGuestById(1365);
-      setSelectedGuest(data)
-    } catch (error) {
-      console.error(error);
+  // fetch single guest
+  useEffect(() => {
+    async function loadSingleGuest() {
+      try {
+        const data = await getGuestById(selectedId);
+        setSelectedGuest(data);
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }
-  loadSingleGuest();
-}, [1365])
-
+    loadSingleGuest();
+  }, [selectedId]);
 
   return (
-  <main className="container">
-  <h1>Guests</h1>
-  <GuestList guests={guests}/>
-  </main>
-);
+    <main className="container">
+      <h1>Guests</h1>
+      <GuestList guests={guests} 
+      setSelectedId={setSelectedId}/>
+    </main>
+  );
 }
 
 export default App;
--- src/components/GuestList.jsx
+++ src/components/GuestList.jsx
@@ -1,26 +1,28 @@
-function GuestList({ guests }) {
-
-    return (
-        <>
-        <h1 className="title">Guest List</h1>
-        {guests.length === 0 ? (
-            <p>No Guests yet...</p>
-        ) : (
-            <ul className="list">
-                {guests.map((guest) => {
-                    return (
-                        <li className="list-item"
-                        key={guest.id}>
-                        {guest.name}
-                        <span>{guest.email}</span>
-                        <span>{guest.phone}</span>
-                        </li>
-                    );
-                })}
-            </ul>
-        )}
-        </>
-    )
+function GuestList({ guests, setSelectedId }) {
+  return (
+    <>
+      <h1 className="title">Guest List</h1>
+      {guests.length === 0 ? (
+        <p>No Guests yet...</p>
+      ) : (
+        <ul className="list">
+          {guests.map((guest) => {
+            return (
+              <li
+                onClick={() => setSelectedId(guest.id)}
+                className="list-item"
+                key={guest.id}
+              >
+                <span>{guest.name}</span>
+                <span>{guest.email}</span>
+                <span>{guest.phone}</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default GuestList;
